076e4d41649eeaf0d01fc77291c9ca2ee9f4552b
diogocastrodev <diogocastro.dev@gmail.com>
2022-05-14 01:14:47 +0100

    Logged users can't go to auth layout

--- web/src/components/Auth/AuthLayout.tsx
+++ web/src/components/Auth/AuthLayout.tsx
@@ -1,48 +1,34 @@
-import Logo from "@public/logoNoBorder.svg";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "@context/AuthContext";
+import { useRouter } from "next/router";
 
 interface props {
   children: React.ReactNode | React.ReactNode[];
 }
 
 export default function AuthLayout({ children }: props) {
+  const { AuthData } = useContext(AuthContext);
+
+  const router = useRouter();
+
+  const checkLogin = () => {
+    if (!AuthData.loading && (AuthData.user || AuthData.is_logged)) {
+      router.push("/");
+    }
+  };
+
+  useEffect(() => {
+    checkLogin();
+  }, [AuthData.loading]);
+
   return (
     <>
-      <div className={``} id="auth-layout"></div>
-      {/*   {Array.from({ length: 20 }).map((_, i) => (
-        <div className="overflow-hidden translate opacity-20">
-          {Array.from({ length: 20 }).map((_, ii) => (
-            <div
-              key={i}
-              style={{
-                paddingRight: "24px",
-                position: "fixed",
-                animationDelay: `${i * 0.1}s`,
-                left: `${256 * ii - 1}px`,
-                top: `${256 * i - 1}px`,
-                width: `${256}px`,
-                transform: "rotate(12deg)",
-                gridRow: `${i}`,
-                gridColumn: `${ii}`,
-              }}
-            >
-              <img src={Logo.src} className={`w-[190px]`} />
-            </div>
-          ))}
-        </div>
-      ))} */}
-      {/* <div className="fixed -bottom-8 -right-8 overflow-hidden opacity-30 translate rotate-12">
-        <img src={Logo.src} />
-      </div>
-      <div className="fixed -bottom-8 -left-8 overflow-hidden opacity-30 translate rotate-12">
-        <img src={Logo.src} />
-      </div>
-      <div className="fixed -top-8 -right-8 overflow-hidden opacity-30 translate rotate-12">
-        <img src={Logo.src} />
-      </div>
-      <div className="fixed -top-8 -left-8 overflow-hidden opacity-30 translate rotate-12">
-        <img src={Logo.src} />
-      </div> */}
-      <div className="z-20">{children}</div>
+      {!AuthData.loading && !AuthData.is_logged && (
+        <>
+          <div className={``} id="auth-layout"></div>
+          <div className="z-20">{children}</div>
+        </>
+      )}
     </>
   );
 }
--- web/src/pages/user/settings/index.tsx
+++ web/src/pages/user/settings/index.tsx
@@ -1,11 +1,45 @@
-import SettingsPageLayout from "@src/components/Settings/SettingsPageLayout";
-import Logo from "@public/logo.svg";
+import SettingsPageLayout from "@components/Settings/SettingsPageLayout";
+import Form from "@components/Form/Form/Form";
+import InputGroup from "@components/Form/Inputs/InputGroup";
+import Input from "@components/Form/Inputs/Input";
+import Label from "@components/Form/Inputs/Label";
+import { AuthContext } from "@context/AuthContext";
+import { useContext, useEffect, useState } from "react";
 
 export default function SettingsPage() {
+  const { AuthData } = useContext(AuthContext);
+
+  const [email, setEmail] = useState<string>("");
+
+  useEffect(() => {
+    if (!AuthData.loading && AuthData.user) {
+      setEmail(AuthData.user!.email);
+    }
+  }, [AuthData.loading || !AuthData.user]);
+
   return (
     <SettingsPageLayout>
       <div>
         <div className="text-xl font-semibold">Alteração de dados</div>
+        <Form
+          method="POST"
+          onSubmit={() => {
+            console.log("submit");
+          }}
+          className="mt-4 space-y-4"
+        >
+          <InputGroup className="">
+            <Label text="Email"></Label>
+            <Input
+              input={{
+                type: "text",
+                className: "w-64",
+                value: email,
+                onChange: (e) => setEmail(e.target.value),
+              }}
+            ></Input>
+          </InputGroup>
+        </Form>
       </div>
     </SettingsPageLayout>
   );
