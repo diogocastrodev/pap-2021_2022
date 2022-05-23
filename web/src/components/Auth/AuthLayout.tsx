dc35552fd9ece72b7ba2ebdf62a54a73e44ee3ef
diogocastrodev <diogocastro.dev@gmail.com>
2022-05-23 12:12:06 +0100

    Go Back on Auth Layout

--- web/src/components/Auth/AuthLayout.tsx
+++ web/src/components/Auth/AuthLayout.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "@context/AuthContext";
 import { useRouter } from "next/router";
+import { ArrowLeftIcon } from "@heroicons/react/outline";
+import Link from "next/link";
 
 interface props {
   children: React.ReactNode | React.ReactNode[];
@@ -26,6 +28,16 @@ export default function AuthLayout({ children }: props) {
       {!AuthData.loading && !AuthData.is_logged && (
         <>
           <div className={``} id="auth-layout"></div>
+          <div
+            className={`fixed top-6 left-6 flex flex-row items-center text-blue-500 hover:text-blue-600 z-50`}
+          >
+            <Link href={`/`}>
+              <div className="cursor-pointer flex items-center">
+                <ArrowLeftIcon className="w-5 h-5" />
+                <span className="ml-1">Voltar</span>
+              </div>
+            </Link>
+          </div>
           <div className="z-20">{children}</div>
         </>
       )}
